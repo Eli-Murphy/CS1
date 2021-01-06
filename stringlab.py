f15364b79f2f38dee59773d5a7b2f6ad4ea3e07d
Eli Murphy <emurphy24@gcds.net>
2021-01-06 11:44:22 -0500

    Create stringlab.py

---
+++ stringlab.py
@@ -0,0 +1,14 @@
+'''
+Created on Jan 6, 2021
+
+@author: elija
+'''
+def main():
+    print("hello")
+    
+    fruit = 'banana'
+    letter = fruit[5]
+    print(letter)
+    
+if __name__ == '__main__':
+    main()
