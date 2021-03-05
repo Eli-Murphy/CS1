4ec9675335f38fc855c8e5cbc9c0a3fc059fa987
Eli Murphy <emurphy24@gcds.net>
2021-03-05 08:52:42 -0500

    Update intothewoods.py

--- Assignments/intothewoods.py
+++ Assignments/intothewoods.py
@@ -5,10 +5,17 @@ Created on Feb 26, 2021
 '''
 def main():
     fhand = open('intothewoods.txt')
+    selectedWord = input("What word would you like to isolate?: ")
+    count = 0
+    lcount = 0
     for line in fhand:
+        lcount = lcount + 1
         line = line.rstrip()
-        if line.find('woods') == -1: continue
-        print(line)
+        if selectedWord in line: 
+            wordperline = int(line.count(selectedWord))
+            count = count + wordperline
+    print("Into The Woods has", str(count), "of your selected word.")
+    print("Line count: ", lcount)
     
 
 if __name__ == '__main__':
