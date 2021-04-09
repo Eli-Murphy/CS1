ae3e498d4b134999d7c16b96d9f4879359b5e606
Eli Murphy <emurphy24@gcds.net>
2021-04-09 07:58:01 -0400

    Create htmltext.py

---
+++ Labs/htmltext.py
@@ -0,0 +1,14 @@
+import re
+import sys
+
+
+def main():
+    hold = "hello\nIt's me\nhola"
+    print("unedited: " + hold)
+    hold2 = repr(hold)
+    print("Edited: " + hold2})
+    
+    
+              
+if __name__ == '__main__':
+    main()
